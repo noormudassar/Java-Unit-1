c2fbfe664db6cf47bb82ec617a5d55d5b9d344b6
Noor Mudassar <noormudassar97@gmail.com>
2023-08-18 20:16:29 -0400

    setting up

---
+++ src/Mars.java
@@ -0,0 +1,9 @@
+public class Mars {
+    public static void main(String[] args) {
+        String colonyName = "MyColony";
+        int shipPopulation = 300;
+        double meals = 4000.00;
+        boolean landing = true;
+    }
+    meals=meals-(shipPopulation*0.75);meals=meals-(shipPopulation*0.75);System.out.println(meals);meals=meals+(meals*.5);shipPopulation=shipPopulation+5;
+}
